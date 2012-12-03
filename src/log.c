54153eedd554db7795be36ecb7ebcd2bfb2ada1c
Andrew Stone <andrew@clovar.com>
2012-12-03 17:48:13 -0500

    Logging: correct data type in formatted string

--- src/log.c
+++ src/log.c
@@ -82,7 +82,7 @@ static void _log(const gchar *log_domain, GLogLevelFlags log_level, const gchar 
 	#if DEBUGGING
 		fprintf(_log_file, "%s - %s : %s\n", log_domain, _get_level(log_level), message);
 	#else
-		fprintf(_log_file, "%s - %s [%ld] %s\n", log_domain, _get_level(log_level), g_get_real_time(), message);
+		fprintf(_log_file, "%s - %s [%" G_GINT64_FORMAT "] %s\n", log_domain, _get_level(log_level), g_get_real_time(), message);
 	#endif
 }
 
