ec31ddc3d39b0d11e945beb7b97d341297634e11
Andrew Stone <andrew@clovar.com>
2012-12-21 13:52:06 -0500

    Monkey: parenthesis around the compares

--- src/monkey.h
+++ src/monkey.h
@@ -12,7 +12,7 @@
  * @return If the string ends with the suffix.
  */
 #define g_string_ends_with(gstring, suffix) \
-	strncmp(gstring->str + gstring->len - MIN((sizeof(suffix) - 1), gstring->len), suffix, sizeof(suffix)-1) == 0
+	(strncmp(gstring->str + gstring->len - MIN((sizeof(suffix) - 1), gstring->len), suffix, sizeof(suffix)-1) == 0)
 
 #ifdef TESTING
 #include "../test/test_monkey.h"
