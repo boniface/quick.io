73852e808e024428782a22bb0620b6682139f8c1
Andrew Stone <andrew@clovar.com>
2014-05-08 12:44:22 -0400

    Adding license header to benches

--- bench/bench_routing.c
+++ bench/bench_routing.c
@@ -1,3 +1,11 @@
+/**
+ * @author Andrew Stone <andrew@clovar.com>
+ * @copyright 2012-2014 Clear Channel Inc.
+ *
+ * This file is part of QuickIO and is released under
+ * the MIT License: http://opensource.org/licenses/MIT
+ */
+
 #include "quickio.h"
 #include <fcntl.h>
 #include <sys/un.h>
